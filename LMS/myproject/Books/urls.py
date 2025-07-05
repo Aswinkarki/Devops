0a08e7feb2f5b954be49d51eb6edb2727c7006c5
Aswinkarki <ashwinkarki124@gmail.com>
2025-07-05 14:44:34 +0545

    removed

--- LMS/myproject/Books/urls.py
+++
@@ -1,8 +0,0 @@
-# books/urls.py
-from django.urls import path
-from .views import BookListView, BookDetailView
-
-urlpatterns = [
-    path('', BookListView.as_view(), name='book-list'),
-    path('<int:book_id>/', BookDetailView.as_view(), name='book-detail'),
-]
--- LMS/myproject/Students/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StudentListCreateView, StudentDetailView
-
-urlpatterns = [
-    path('students/', StudentListCreateView.as_view(), name='student-list'),
-    path('students/<int:student_id>/', StudentDetailView.as_view(), name='student-detail'),
-]
